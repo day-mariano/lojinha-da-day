c398fc16178ddf6df099bc08ea0cccb2ebfea723
Dayana Mariano <dayanaraquelle@gmail.com>
2024-03-17 15:49:35 -0300

    feat: ProductList

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { ProductList } from "./components/product-list";
 
 function App() {
-  return <ProductList />;
+  return (
+    <main className="flex justify-center">
+      <div className="p-4 max-w-screen-xl">
+        <h2 className="text-4xl font-bold mb-4">Produtos</h2>
+        <ProductList />
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -1,5 +1,43 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
 
 export const ProductList: FC = () => {
-  return <div>ProductList</div>;
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((res) => {
+        setProducts(res);
+      });
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {products.map((product) => (
+        <article key={product.id}>
+          <Card className="flex">
+            <div className="flex shrink-0 justify-center w-[120px] h-[120px] p-4">
+              <img
+                className="object-contain"
+                src={product.image}
+                alt={product.title}
+              />
+            </div>
+            <CardHeader>
+              <CardTitle>{product.title}</CardTitle>
+              <CardDescription>${product.price.toFixed(2)}</CardDescription>
+            </CardHeader>
+          </Card>
+        </article>
+      ))}
+    </div>
+  );
 };
