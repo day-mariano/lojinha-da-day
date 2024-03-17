b72efe11545d814358370f954195c6d745258c34
Dayana Mariano <dayanaraquelle@gmail.com>
2024-03-17 16:29:18 -0300

    feat: ProductDetails

--- src/components/product-details.tsx
+++ src/components/product-details.tsx
@@ -1,5 +1,32 @@
 import { FC } from "react";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Product } from "./product-list";
 
-export const ProductDetails: FC = () => {
-  return <div>ProductDetails</div>;
+interface ProductDetailsProps {
+  product: Product;
+}
+
+export const ProductDetails: FC<ProductDetailsProps> = ({ product }) => {
+  return (
+    <DialogContent className="flex justify-center">
+      <div className="w-[120px] h-[120px] shrink-0">
+        <img src={product.image} alt={product.title} />
+      </div>
+      <div>
+        <DialogHeader>
+          <DialogTitle>{product.title}</DialogTitle>
+          <DialogDescription>
+            ${product.price.toFixed(2)} ⭐️ {product.rating.rate}/5 de{" "}
+            {product.rating.count} avaliações
+          </DialogDescription>
+        </DialogHeader>
+        <p>{product.description}</p>
+      </div>
+    </DialogContent>
+  );
 };
--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -1,15 +1,24 @@
 import { FC, useEffect, useState } from "react";
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { ProductDetails } from "./product-details";
+import { Dialog, DialogTrigger } from "./ui/dialog";
 
-interface Product {
+export interface Product {
   id: number;
   title: string;
   price: number;
+  description: string;
+  category: string;
   image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
 }
 
 export const ProductList: FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<Product>();
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -20,24 +29,33 @@ export const ProductList: FC = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {products.map((product) => (
-        <article key={product.id}>
-          <Card className="flex">
-            <div className="flex shrink-0 justify-center w-[120px] h-[120px] p-4">
-              <img
-                className="object-contain"
-                src={product.image}
-                alt={product.title}
-              />
-            </div>
-            <CardHeader>
-              <CardTitle>{product.title}</CardTitle>
-              <CardDescription>${product.price.toFixed(2)}</CardDescription>
-            </CardHeader>
-          </Card>
-        </article>
-      ))}
-    </div>
+    <Dialog>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {products.map((product) => (
+          <DialogTrigger
+            key={product.id}
+            className="text-left flex w-full"
+            onClick={() => setSelectedProduct(product)}
+          >
+            <article className="flex-grow">
+              <Card className="flex">
+                <div className="flex shrink-0 justify-center w-[120px] h-[120px] p-4">
+                  <img
+                    className="object-contain"
+                    src={product.image}
+                    alt={product.title}
+                  />
+                </div>
+                <CardHeader>
+                  <CardTitle>{product.title}</CardTitle>
+                  <CardDescription>${product.price.toFixed(2)}</CardDescription>
+                </CardHeader>
+              </Card>
+            </article>
+          </DialogTrigger>
+        ))}
+        {selectedProduct && <ProductDetails product={selectedProduct} />}
+      </div>
+    </Dialog>
   );
 };
