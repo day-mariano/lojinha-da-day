670e1c295e81b827e1e8a87171665e4a715406a4
Dayana Mariano <dayanaraquelle@gmail.com>
2024-03-17 21:18:09 -0300

    feat: update icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 import { ProductList } from "./components/product-list";
 
 function App() {
   return (
     <main className="flex flex-col justify-center items-center">
       <header className="flex items-center bg-rose-50 text-2xl justify-around border-b-2 h-24 w-full">
-        <img className="flex w-32 mt-4" src="/src/assets/1-removebg-preview.png" alt="" />
+        <img className="flex w-32 mt-4" src="public/1-removebg-preview.png" alt="" />
         <nav>
         <a href="https://www.linkedin.com/in/dayana-mariano/" target="_blank" className="text-lg p-2 rounded hover:bg-red-200">Linkedin</a>
         <a href="https://github.com/day-mariano" target="_blank" className="text-lg p-2 ml-3 rounded hover:bg-red-200">GitHub</a>
